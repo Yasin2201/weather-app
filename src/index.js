5d59b98d3a46725eec11b1ff7e6302adde16b96b
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-04-15 20:03:08 +0100

    initiate onClick

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('working')
+import printAddress from './DOM'
+import { getWeather, getUserInput } from './weather'
+
+
+const searchBtn = document.querySelector('#searchBtn')
+
+searchBtn.addEventListener('click', () => {
+    const result = getWeather(getUserInput())
+    printAddress(result);
+
+})
+
